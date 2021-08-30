9f6d6b3cf42db51e009bf05e0ceee98eeec65665
zahid hasan <hzahid2999@gmail.com>
2021-08-30 21:20:37 +0600

    added route and upload file in firebase

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,6 +18,7 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
 
@@ -24,4 +26,5 @@ export {
   auth,
   db,
   usersCollection,
+  storage,
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Manage from '../views/Manage.vue';
+import store from '@/store';
 
 const routes = [
   {
@@ -10,16 +13,49 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
+  },
+  {
+    path: '/manage-music',
+    name: 'Manage',
+    meta: {
+      requiresAuth: true,
+    },
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'Manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'Home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  // console.log(to.matched);
+
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'Home' });
+  }
 });
 
 export default router;
